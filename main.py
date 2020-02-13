4d97343366a80e91d926d187814fbec391ba1690
ethru <ethru@protonmail.com>
2020-02-13 13:35:00 +0100

    [Add documentation for main.py]

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
+#!/usr/bin/env python3
+"""MemorizeIT
+
+This is main file of MemorizeIT game. Project is created to increase memory and focus among children or adults who would
+like to take part in training. Rules of game are quite simple. You just need to count elements displayed on screen and
+remember their type. At the end you will be asked to write down your results and confront them with exact ones. This
+will be verified by marking your answer with proper color (correct - green, not - red). Difficulty of game can be
+increased by adding more types of elements to count, mixing their colors, turning sound off or changing time dedicated
+to each wave. It is also more difficult to play normal game mode (instead of static), because figures are moving which
+is additional distraction. I wish you enjoyable experience with game and best results in memory training. Good luck!
+
+Side note: in installation directory there is place called elements where you can add your custom pictures which
+will be displayed during static mode gameplay. Application scales them automatically, so you don't have to worry about
+it. If you don't like custom graphics just remove them from elements directory then figures will be displayed.
+
+License:
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import pygame
 import gui
 
 
 def main():
+    """Initializes main menu of the application"""
+
     pygame.init()
     pygame.display.set_caption('MemorizeIT')
     try:
